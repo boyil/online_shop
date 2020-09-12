5339d4b9744334062c0cfa69f591c3aad7370f75
boyil <boyi.l.dimi@gmail.com>
2020-09-12 11:02:53 -0700

    Add db configs

--- src/main/java/onlineShop/ApplicationConfig.java
+++ src/main/java/onlineShop/ApplicationConfig.java
@@ -1,21 +1,41 @@
 package onlineShop;
 
-import onlineShop.log.Logger;
-import onlineShop.log.PaymentAction;
-import onlineShop.log.ServerLogger;
+import java.util.Properties;
+import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 
 @Configuration
 public class ApplicationConfig {
 
-    @Bean
-    public Logger getLogger() {
-        return new ServerLogger();
+    @Bean(name = "sessionFactory")
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource());
+        sessionFactory.setPackagesToScan("onlineShop.model");
+        sessionFactory.setHibernateProperties(hibernateProperties());
+        return sessionFactory;
     }
 
-    @Bean
-    public PaymentAction paymentAction() {
-        return new PaymentAction();
+    @Bean(name = "dataSource")
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+
+        // TODO: Put in your RDS_IP and USERNAME and PASSWORD
+        dataSource.setUrl("jdbc:mysql://YOUR_RDS_IP:3306/ecommerce?createDatabaseIfNotExist=true&serverTimezone=UTC");
+        dataSource.setUsername("USERNAME");
+        dataSource.setPassword("PASSWORD");
+
+        return dataSource;
+    }
+
+    private final Properties hibernateProperties() {
+        Properties hibernateProperties = new Properties();
+        hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
+        hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
+        return hibernateProperties;
     }
 }
